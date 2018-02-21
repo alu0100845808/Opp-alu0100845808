9c3ae672b6e74e9ecadaff9ae95b2ba5ab6a3edf
alu0100845808 <alu0100845808@ull.edu.es>
2018-02-21 20:37:38 +0000

    probando mocha

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var UnderlinedCell = require("UnderlinedCell")
+var Cell = require("Cell")
+var RCell = require("RCell")
+var Table = require("Table");
+var assert = require('assert');
+
+describe('Cell', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
