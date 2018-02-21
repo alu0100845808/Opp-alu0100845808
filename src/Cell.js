c45857bcc3fd73634094e7aa166c06bf271fa17c
alu0100845808 <alu0100845808@ull.edu.es>
2018-02-21 15:38:21 +0000

    Modificada celda

--- src/Cell.js
+++ src/Cell.js
@@ -1,35 +1,25 @@
+class TCell {
+  constructor(text) {
+    this.text = text.split("\n");
+}
 
-function repeat(string, times) {  //duplica un strin x veces ejemplo repeat(hola,2)->holahola
-    var result = "";
-    for (var i = 0; i < times; i++)
-        result += string;
+draw = function(width, height) { //crea la cell
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || ""; //coje el texto en caso de que exista sino pone vacio
+        result.push(line + " ".repeat(width - line.length)); //pone tantos espacios como necesite para llegar al minwith
+    }
     return result;
-}
+};
 
-function Cell(text) { //Constructor de celdas 
-    this.text = text.split("\n"); //separa el texto por saltos de linea 
-}
-
-Cell.prototype.minWidth = function() {
+minWidth = function() {
     return this.text.reduce(function(minwidthrelative, line) { // bucle que busca el minimo absoluto Ej mas sencillos ->         //var numbers = [65, 44, 12, 4];//function getSum(maximorelativo, num) {	//document.write(total + " " + num + " "); //return Math.max(total, num);//}//function myFunction(item) {//document.getElementById("demo").innerHTML = numbers.reduce(getSum);//}
         return Math.max(minwidthrelative, line.length);
     }, 0);
 };
 
-Cell.prototype.minHeight = function() {
+minHeight = function() {
     return this.text.length; //numero de columnas del array text
 };
 
-Cell.prototype.draw = function(width, height) { //crea la cell
-    var result = [];
-    for (var i = 0; i < height; i++) {
-        var line = this.text[i] || ""; //coje el texto en caso de que exista sino pone vacio
-        result.push(line + repeat(" ", width - line.length)); //pone tantos espacios como necesite para llegar al minwith
-    }
-    return result;
-};
-
-module.exports = {
-    Cell: Cell,
-    repeat: repeat
 }
